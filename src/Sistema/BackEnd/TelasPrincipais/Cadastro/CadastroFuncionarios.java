b0c3997d390bc67bc35f153141454dcce15f9f14
fabricio.washington <fabricio.washington@AN0900104061954.dinfor.uniceub>
2024-09-30 10:09:23 -0300

    Adicionando a inserção do cadastro de funcionarios

--- src/Sistema/BackEnd/TelasPrincipais/Cadastro/CadastroFuncionarios.java
+++ src/Sistema/BackEnd/TelasPrincipais/Cadastro/CadastroFuncionarios.java
@@ -1,8 +1,12 @@
 package Sistema.BackEnd.TelasPrincipais.Cadastro;
 
 import DAO.Cadastro.CadastroFuncionarioDAO;
+import DTO.Cadastro.CadastroFuncionarioDTO;
 import Sistema.BackEnd.Validacoes;
 import java.util.Date;
+import javax.swing.JFormattedTextField;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 public class CadastroFuncionarios {
 
@@ -13,89 +17,93 @@ public class CadastroFuncionarios {
     private Validacoes validador;
     private CadastroUsuarios metodoObterHorario;
     private CadastroFuncionarioDAO cadastroFuncionariosDAO;
+    private CadastroFuncionarioDTO cadastroFuncionariosDTO;
 
     public CadastroFuncionarios() {
         this.cadastroFuncionariosDAO = new CadastroFuncionarioDAO();
         this.validador = new Validacoes();
+        this.cadastroFuncionariosDTO = new CadastroFuncionarioDTO();
         this.metodoObterHorario = new CadastroUsuarios();
         metodoObterHorario.obterDataAtual();
 
     }
 
     public CadastroFuncionarios(String cpf, String nomeCompleto) {
+        this();
         this.cpf = cpf;
         this.nomeCompleto = nomeCompleto;
 
     }
 
-    /**
-     * @return the nomeCompleto
-     */
+    public void cadastrar() {
+        if (!validarCampos()) {
+            JOptionPane.showMessageDialog(null, "Por favor, preencha todos os campos!", "Erro", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        if (!validador.validarCpf(cpf)) {
+            JOptionPane.showMessageDialog(null, "CPF inválido!", "Erro", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        // Formatar o CPF
+        cpf = validador.formatarCpf(cpf);
+
+        cadastroFuncionariosDTO.setCPF(cpf);
+        cadastroFuncionariosDTO.setNome_Completo(nomeCompleto);
+        cadastroFuncionariosDTO.setData_Cadastro(dataCadastro);
+        cadastroFuncionariosDTO.setData_Modificacao(dataModificacao);
+
+        cadastroFuncionariosDAO.cadastrarFuncionarioCompleto(cadastroFuncionariosDTO);
+
+    }
+
     public String getNomeCompleto() {
         return nomeCompleto;
     }
 
-    /**
-     * @param nomeCompleto the nomeCompleto to set
-     */
     public void setNomeCompleto(String nomeCompleto) {
         this.nomeCompleto = nomeCompleto;
     }
 
-    /**
-     * @return the cpf
-     */
     public String getCpf() {
         return cpf;
     }
 
-    /**
-     * @param cpf the cpf to set
-     */
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
 
-    /**
-     * @return the dataCadastro
-     */
     public Date getDataCadastro() {
         return dataCadastro;
     }
 
-    /**
-     * @param dataCadastro the dataCadastro to set
-     */
     public void setDataCadastro(Date dataCadastro) {
         this.dataCadastro = dataCadastro;
     }
 
-    /**
-     * @return the dataModificacao
-     */
     public Date getDataModificacao() {
         return dataModificacao;
     }
 
-    /**
-     * @param dataModificacao the dataModificacao to set
-     */
     public void setDataModificacao(Date dataModificacao) {
         this.dataModificacao = dataModificacao;
     }
 
-    /**
-     * @return the validador
-     */
     public Validacoes getValidador() {
         return validador;
     }
 
-    /**
-     * @param validador the validador to set
-     */
     public void setValidador(Validacoes validador) {
         this.validador = validador;
+    }
+
+    public boolean validarCampos() {
+        return !cpf.isEmpty() && !nomeCompleto.isEmpty();
+    }
+
+    public void limparCampos(JFormattedTextField txtCpf, JTextField txtNomeCompleto) {
+        txtCpf.setText("");
+        txtNomeCompleto.setText("");
     }
 
 }
