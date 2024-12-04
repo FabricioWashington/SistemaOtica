f8cf5c6f34ef1cc39042308ec0618b07c638cba9
FabricioWashington <fabriciowashington2015@gmail.com>
2024-12-04 16:23:55 -0300

    Criando classe DTO para tabela de tipo_pagamento

---
+++ src/DTO/Vetores/TipoPagamentoDTO.java
@@ -0,0 +1,36 @@
+package DTO.Vetores;
+
+
+public class TipoPagamentoDTO {
+    private int idPagamento;
+    private String tipoPagamento;
+
+    /**
+     * @return the idPagamento
+     */
+    public int getIdPagamento() {
+        return idPagamento;
+    }
+
+    /**
+     * @param idPagamento the idPagamento to set
+     */
+    public void setIdPagamento(int idPagamento) {
+        this.idPagamento = idPagamento;
+    }
+
+    /**
+     * @return the tipoPagamento
+     */
+    public String getTipoPagamento() {
+        return tipoPagamento;
+    }
+
+    /**
+     * @param tipoPagamento the tipoPagamento to set
+     */
+    public void setTipoPagamento(String tipoPagamento) {
+        this.tipoPagamento = tipoPagamento;
+    }
+    
+}
